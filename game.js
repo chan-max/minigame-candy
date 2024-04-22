7c1ce1b86e84318bb1eb6f4302fc2a53a595c85c
chan-max <jackieontheway666@gmail.com>
2024-04-22 19:58:21 +0800

    dev

---
+++ game.js
@@ -0,0 +1,13 @@
+const core = require('./main.js')
+
+let systemInfo = tt.getSystemInfoSync();
+let canvas = tt.createCanvas();
+canvas.width = systemInfo.windowWidth;
+canvas.height = systemInfo.windowHeight;
+
+// let ctx = canvas.getContext('2d');
+function draw() {
+  core.createGame(canvas._canvas)
+}
+
+draw();
